8eba984eceb8fc39e51f285def88b6324868aed0
Shivansh2287 <54775060+Shivansh2287@users.noreply.github.com>
2021-08-31 14:03:03 +0530

    production

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA59GW3S2jmxTYocF3EhAXxYghXS_ihlbo",
@@ -8,3 +10,10 @@ const firebaseConfig = {
   appId: "1:422677948528:web:52694e11f37d3480a12cb5",
   measurementId: "G-07J7QL01TH",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
